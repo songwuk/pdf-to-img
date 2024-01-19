f6e495dc641a7609f70f2061d88dc968d4f10d86
songwuk <1017301325@qq.com>
2024-01-19 17:04:51 +0800

    upgrade

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
+import pdfLogo from '/pdf.svg'
 import * as pdfjs from 'pdfjs-dist'
 const pdfjsWorker = await import('pdfjs-dist/build/pdf.worker.min.mjs');
 pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker
 document.querySelector('#app').innerHTML = `
   <div style="display: flex">
-    <div>
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="${viteLogo}" class="logo" alt="Vite logo" />
-      </a>
+    <div style="display: flex;align-items: center;">
       <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-        <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+        <img src="${pdfLogo}" class="logo vanilla" alt="JavaScript logo" />
       </a>
       <div>
         <input id="upload" type="file">
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
 
 import { defineConfig } from 'vite'
-import topLevelAwait from "vite-plugin-top-level-await";
+// import topLevelAwait from "vite-plugin-top-level-await";
 export default defineConfig({
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext',
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    target: 'esnext',
+  },
   plugins: [
-    topLevelAwait({
-      // The export name of top-level await promise for each chunk module
-      promiseExportName: "__tla",
-      // The function to generate import names of top-level await promise in each chunk module
-      promiseImportName: i => `__tla_${i}`
-    })
+    // topLevelAwait({
+    //   // The export name of top-level await promise for each chunk module
+    //   promiseExportName: "__tla",
+    //   // The function to generate import names of top-level await promise in each chunk module
+    //   promiseImportName: i => `__tla_${i}`
+    // })
   ]
 })
